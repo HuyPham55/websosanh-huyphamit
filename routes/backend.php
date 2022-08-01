0bd727ef98790e5cbca4c8560e225ccadd46633b
huypham55 <huyducphamofficial@gmail.com>
2022-08-01 14:34:23 +0700

    Added translation manager composer require barryvdh/laravel-translation-manager php artisan vendor:publish --provider="Barryvdh\TranslationManager\ManagerServiceProvider" --tag=migrations, --tag=config,  --tag=views

--- routes/backend.php
+++ routes/backend.php
@@ -1,1 +1,8 @@
 <?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function () {
+    Route::get('dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
+});
