f36d723637a87ee8876e00e1769ec1b83c253653
huydu <huyducphamofficial@gmail.com>
2023-04-21 10:41:38 +0700

    Fix: Target class [] does not exist.

--- routes/frontend.php
+++ routes/frontend.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => ''], function () {
-    Route::any('/{any}', function () {
-        return view('frontend.app');
-    })
-        ->where('any', '.*');
-});
+Route::any('/{any}', function () {
+    return view('frontend.app');
+})
+    ->where('any', '.*');
