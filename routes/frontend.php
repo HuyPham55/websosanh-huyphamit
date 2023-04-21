d7b63884564c97f5021b7a9d68395d41357d8704
huydu <huyducphamofficial@gmail.com>
2023-04-21 10:43:05 +0700

    Fix: Target class [] does not exist.

--- routes/frontend.php
+++ routes/frontend.php
@@ -3,11 +3,10 @@
 use App\Http\Controllers\Frontend\ContactController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => ''], function () {
-    //Contact
-    Route::group(['prefix' => 'contact'], function () {
-        Route::get('/', [ContactController::class, 'getContactPage'])->name('contact.index');
-        Route::post('/', [ContactController::class, 'postContact'])->name('contact.post');
-        Route::post('/refresh-captcha', [ContactController::class, 'refreshCaptcha'])->name('contact.refresh_captcha');
-    });
+
+//Contact
+Route::group(['prefix' => 'contact'], function () {
+    Route::get('/', [ContactController::class, 'getContactPage'])->name('contact.index');
+    Route::post('/', [ContactController::class, 'postContact'])->name('contact.post');
+    Route::post('/refresh-captcha', [ContactController::class, 'refreshCaptcha'])->name('contact.refresh_captcha');
 });
