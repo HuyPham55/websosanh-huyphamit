267bd335a28b0c5aad575f4cc954737c80627215
huydu <huyducphamofficial@gmail.com>
2023-04-21 09:53:13 +0700

    Feat: (UX) Make localization global middleware

--- routes/frontend.php
+++ routes/frontend.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Frontend\ContactController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => 'localization'], function () {
+Route::group(['middleware' => ''], function () {
     //Contact
     Route::group(['prefix' => 'contact'], function () {
         Route::get('/', [ContactController::class, 'getContactPage'])->name('contact.index');
