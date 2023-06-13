6e4ab6a810bd5eb4920fd4270f496ff460408422
huydu <huyducphamofficial@gmail.com>
2023-06-13 22:33:25 +0700

    Feat: Update SEO for homepage

--- resources/views/frontend/app.blade.php
+++ resources/views/frontend/app.blade.php
@@ -12,6 +12,33 @@
     <link rel="shortcut icon" href="{{ asset(cachedOption('site_favicon')) }}"/>
     <script src="/js/swiper.min.js "></script>
 
+    <!-- meta -->
+    <meta name="title" content="{{cachedOption("site_seo_title_".app()->getLocale())}}"/>
+    <meta name="description" content="{{cachedOption("site_description_".app()->getLocale())}}">
+    <meta name="image" content="{{normalizeImageUrl(cachedOption('site_logo'))}}">
+
+    <!-- Open Graph -->
+    <meta property="og:title" content="{{cachedOption("site_seo_title_".app()->getLocale())}}"/>
+    <meta property="og:url" content="{{ url()->full() }}"/>
+    <meta property="og:image" content="{{normalizeImageUrl(cachedOption('site_logo'))}}"/>
+    <meta property="og:description" content="{{cachedOption("site_description_".app()->getLocale())}}"/>
+
+    <!-- Schema.org markup for Google+ -->
+    <meta itemprop="name" content="{{cachedOption("site_seo_title_".app()->getLocale())}}">
+    <meta itemprop="description" content="{{cachedOption("site_description_".app()->getLocale())}}">
+    <meta itemprop="image" content="{{normalizeImageUrl(cachedOption('site_logo'))}}">
+
+    {{--Twitter--}}
+    <meta name="twitter:title" content="{{cachedOption("site_seo_title_".app()->getLocale())}}">
+    <meta name="twitter:description" content="{{cachedOption("site_description_".app()->getLocale())}}">
+    <meta name="twitter:image" content="{{normalizeImageUrl(cachedOption('site_logo'))}}">
+
+    <meta name="rating" content="general"/>
+    <meta name="robots" content="all"/>
+    <meta name="robots" content="index, follow"/>
+    <meta name="revisit-after" content="1 days"/>
+
+    <link rel="canonical" href="{{ url()->full() }}">
 
     @vite(['resources/sass/style.scss'])
 </head>
